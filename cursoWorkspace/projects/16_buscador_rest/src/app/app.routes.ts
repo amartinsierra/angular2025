e76982b9a8ee55893a7c3adc6afa2f3c3ecc43dc
Antonio Martín <ajms66@gmail.com>
2025-05-14 14:28:18 +0200

    14-5-2025  14:30

--- cursoWorkspace/projects/16_buscador_rest/src/app/app.routes.ts
+++ cursoWorkspace/projects/16_buscador_rest/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { AltaComponent } from './components/alta/alta.component';
+import { BuscarComponent } from './components/buscar/buscar.component';
+import { EliminarComponent } from './components/eliminar/eliminar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:"agregar",
+    component:AltaComponent
+  },
+  {
+    path:"buscar",
+    component:BuscarComponent
+  },
+  {
+    path:"eliminar",
+    component:EliminarComponent
+  }
+];
---
+++ cursoWorkspace/projects/16_buscador_rest/src/app/service/buscador.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Item } from '../model/Item';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BuscadorService {
+
+
+  urlBase:string = 'http://localhost:8000/buscador';
+
+  constructor(private http:HttpClient) { }
+
+  buscar(tematica:string):Observable<Item[]>{
+     /*const url_busqueda = `${this.urlBase}/buscar?tematica=${tematica}`;
+     return this.http.get<Item[]>(url_busqueda);*/
+     const url_busqueda = `${this.urlBase}/buscar`;
+     return this.http.get<Item[]>(url_busqueda,{params:{"tematica":tematica}});
+  }
+
+  alta(item:Item):Observable<void>{
+    console.log(JSON.stringify(item))
+    const url_alta = `${this.urlBase}/alta`;
+    let head=new HttpHeaders();
+    head.set("Content-Type","application/json")
+    return this.http.post<void>(url_alta, item,{headers:head});
+  }
+
+  eliminar(tematica:string):Observable<Item[]>{
+    const url_eliminar=`${this.urlBase}/eliminar?tematica=${tematica}`;
+    return this.http.delete<Item[]>(url_eliminar);
+  }
+
+
+
+}
