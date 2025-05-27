d5a522c044a03f061d1088542cabe59a695624a8
Antonio Martín <ajms66@gmail.com>
2025-05-27 09:48:40 +0200

    27_5_2025 9:45

---
+++ cursoWorkspace/projects/19_altas_contactos/src/app/service/contactos.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, map, Observable, of } from 'rxjs';
+import { Contacto } from '../model/Contacto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactosService {
+  url:string="http://localhost:3000/contactos";
+  constructor(private http:HttpClient) { }
+
+  alta(contacto:Contacto):Observable<boolean>{
+    return this.http.post(this.url+"/alta",contacto);
+
+  }
+}
