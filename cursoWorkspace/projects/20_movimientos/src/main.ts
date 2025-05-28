709ae2ac6548f5a3f44bf9e041619c4cf934cb36
Antonio Martín <ajms66@gmail.com>
2025-05-28 09:58:59 +0200

    28-5-2025  10:00

--- cursoWorkspace/projects/20_movimientos/src/main.ts
+++ cursoWorkspace/projects/20_movimientos/src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { MovimientosComponent } from './app/components/movimientos/movimientos.component';
 
-bootstrapApplication(AppComponent, appConfig)
+
+bootstrapApplication(MovimientosComponent, appConfig)
   .catch((err) => console.error(err));
