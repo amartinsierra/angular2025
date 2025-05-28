fad69859ee88692a08beaa5a86705174f1e11ddd
Antonio Martín <ajms66@gmail.com>
2025-05-28 09:41:25 +0200

    28-5-2025 9:40

--- cursoWorkspace/projects/19_altas_contactos/src/app/components/alta/alta.component.ts
+++ cursoWorkspace/projects/19_altas_contactos/src/app/components/alta/alta.component.ts
@@ -11,17 +11,28 @@ import { FormsModule } from '@angular/forms';
 })
 export class AltaComponent {
   mensaje:string;
+  fecha:Date;
   contacto:Contacto=new Contacto();
   constructor(private contactosService:ContactosService){
 
   }
   alta(){
-    this.contactosService.alta(this.contacto).subscribe({
+    console.log(this.fecha);
+    //si el error se controla aquí en el componente
+    /*this.contactosService.alta(this.contacto).subscribe({
         next: data=>this.mensaje="Contacto agregado",
         error: err=>this.mensaje="No se pudo dar de alta el contacto!!!"
       }
-
-    );
+    );*/
+    //si el error se ha capturado en el service
+    this.contactosService.alta(this.contacto)
+    .subscribe(data=>{
+      if(data){
+        this.mensaje="Contacto agregado";
+      }else{
+        this.mensaje="No se pudo dar de alta el contacto!!!";
+      }
+    })
 
   }
 }
--- cursoWorkspace/projects/19_altas_contactos/src/app/service/contactos.service.ts
+++ cursoWorkspace/projects/19_altas_contactos/src/app/service/contactos.service.ts
@@ -9,9 +9,15 @@ import { Contacto } from '../model/Contacto';
 export class ContactosService {
   url:string="http://localhost:3000/contactos";
   constructor(private http:HttpClient) { }
-
-  alta(contacto:Contacto):Observable<boolean>{
+  //el posible error 409 se captura en el componente
+  /*alta(contacto:Contacto):Observable<any>{
     return this.http.post(this.url+"/alta",contacto);
 
+  }*/
+  //el posible error 409 se captura aquí en el service
+  alta(contacto:Contacto):Observable<boolean>{
+    return this.http.post(this.url+"/alta",contacto)
+    .pipe(map(()=>true),
+              catchError(e=>of(false)));
   }
 }
