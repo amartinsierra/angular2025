1477df350a6cf61d23505d53a62530a190080146
Antonio Martín <ajms66@gmail.com>
2025-06-05 12:51:12 +0200

    5-6-2025 12:50

--- cursoWorkspace/projects/22_matriculaciones/src/app/service/matriculas.service.ts
+++ cursoWorkspace/projects/22_matriculaciones/src/app/service/matriculas.service.ts
@@ -3,21 +3,22 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Matricula } from '../model/Matricula';
 import { Curso } from '../model/Curso';
+import { urlBase } from '../utilities/Constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MatriculasService {
-  urlBase:string = 'http://localhost:3000/matriculacion';
+
   constructor(private http:HttpClient) { }
 
   buscarMatriculas(idCurso:number):Observable<Matricula[]>{
-    const url:string =`${this.urlBase}/matriculas/${idCurso}`;
+    const url:string =`${urlBase}/matriculas/${idCurso}`;
     return this.http.get<Matricula[]>(url);
   }
 
   buscarCursos():Observable<Curso[]>{
-    const url:string =`${this.urlBase}/cursos`;
+    const url:string =`${urlBase}/cursos`;
     return this.http.get<Curso[]>(url);
   }
 
